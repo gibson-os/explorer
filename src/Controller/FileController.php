33091e7edf61a3157d8d2db396f0d8d5f43a3573
Benjamin Wollenweber <ich@wollis-page.de>
2020-11-12 23:22:51 +0100

    Add file download action

--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -4,17 +4,24 @@ declare(strict_types=1);
 namespace GibsonOS\Module\Explorer\Controller;
 
 use GibsonOS\Core\Controller\AbstractController;
+use GibsonOS\Core\Exception\DateTimeError;
 use GibsonOS\Core\Exception\FactoryError;
 use GibsonOS\Core\Exception\GetError;
 use GibsonOS\Core\Exception\LoginRequired;
 use GibsonOS\Core\Exception\PermissionDenied;
+use GibsonOS\Core\Exception\Repository\SelectError;
 use GibsonOS\Core\Exception\Sqlite\ExecuteError;
 use GibsonOS\Core\Exception\Sqlite\ReadError;
 use GibsonOS\Core\Exception\Sqlite\WriteError;
 use GibsonOS\Core\Repository\SettingRepository;
 use GibsonOS\Core\Service\PermissionService;
+use GibsonOS\Core\Service\RequestService;
 use GibsonOS\Core\Service\Response\AjaxResponse;
+use GibsonOS\Core\Service\Response\FileResponse;
+use GibsonOS\Core\Service\Response\ResponseInterface;
 use GibsonOS\Core\Service\ServiceManagerService;
+use GibsonOS\Core\Service\SessionService;
+use GibsonOS\Core\Service\TwigService;
 use GibsonOS\Core\Utility\StatusCode;
 use GibsonOS\Module\Explorer\Factory\File\Type\DescriberFactory;
 use GibsonOS\Module\Explorer\Service\GibsonStoreService;
@@ -22,27 +29,54 @@ use GibsonOS\Module\Explorer\Service\TrashService;
 
 class FileController extends AbstractController
 {
+    /**
+     * @var SettingRepository
+     */
+    private $settingRepository;
+
+    public function __construct(
+        PermissionService $permissionService,
+        RequestService $requestService,
+        TwigService $twigService,
+        SessionService $sessionService,
+        SettingRepository $settingRepository
+    ) {
+        parent::__construct($permissionService, $requestService, $twigService, $sessionService);
+        $this->settingRepository = $settingRepository;
+    }
+
     public function delete(
         TrashService $trashService,
-        SettingRepository $settingRepository,
         string $dir,
         array $files
     ): AjaxResponse {
         $this->checkPermission(PermissionService::DELETE);
-
-        $homePath = $settingRepository->getByKeyAndModuleName(
-            'explorer',
-            $this->sessionService->getUserId() ?? 0,
-            'home_path'
-        )->getValue();
-
-        if (mb_strpos($homePath, $dir) === 0) {
+        if (mb_strpos($this->getHomePath(), $dir) === 0) {
             return $this->returnFailure('Access denied', StatusCode::FORBIDDEN);
         }
 
         $trashService->add($dir, $files);
 
         return $this->returnSuccess();
+    }
+
+    /**
+     * @throws DateTimeError
+     * @throws LoginRequired
+     * @throws PermissionDenied
+     * @throws SelectError
+     */
+    public function download(RequestService $requestService): ResponseInterface
+    {
+        $this->checkPermission(PermissionService::READ);
+
+        $filename = '/' . urldecode($requestService->getQueryString());
+
+        if (mb_strpos($this->getHomePath(), $filename) === 0) {
+            return $this->returnFailure('Access denied', StatusCode::FORBIDDEN);
+        }
+
+        return new FileResponse($requestService, $filename);
     }
 
     /**
@@ -73,5 +107,18 @@ class FileController extends AbstractController
         $gibsonStoreService->setFileMetas($path, $fileMetas, $checkSum ?: null);
 
         return $this->returnSuccess($fileMetas);
+    }
+
+    /**
+     * @throws DateTimeError
+     * @throws SelectError
+     */
+    private function getHomePath(): string
+    {
+        return $this->settingRepository->getByKeyAndModuleName(
+            'explorer',
+            $this->sessionService->getUserId() ?? 0,
+            'home_path'
+        )->getValue();
     }
 }
