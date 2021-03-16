4c4afe6099688b7d00f5ce37b2e2155066c492e4
Benjamin Wollenweber <ich@wollis-page.de>
2021-03-16 23:12:24 +0100

    Add directory parameter

---
+++ assets/js/dir/Parameter.js
@@ -0,0 +1,22 @@
+Ext.define('GibsonOS.module.explorer.dir.Parameter', {
+    extend: 'GibsonOS.module.core.component.form.FieldContainer',
+    alias: ['widget.gosModuleExplorerDirParameter'],
+    initComponent() {
+        const me = this;
+
+        me.items = [{
+            xtype: 'gosFormTextfield',
+            name: me.name,
+            margins: '0 5 0 0'
+        },{
+            xtype: 'gosButton',
+            text: '...',
+            flex: 0,
+            handler: function() {
+                GibsonOS.module.explorer.dir.fn.dialog(me.down('gosFormTextfield'));
+            }
+        }];
+
+        me.callParent();
+    }
+});
---
+++ src/Dto/Parameter/Directory.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace GibsonOS\Module\Explorer\Dto\Parameter;
+
+use GibsonOS\Core\Dto\Parameter\AbstractParameter;
+
+class Directory extends AbstractParameter
+{
+    public function __construct(string $title)
+    {
+        parent::__construct($title, 'gosModuleExplorerDirParameter');
+    }
+
+    protected function getTypeConfig(): array
+    {
+        return [];
+    }
+
+    public function getAllowedOperators(): array
+    {
+        return [
+            self::OPERATOR_EQUAL,
+            self::OPERATOR_NOT_EQUAL,
+        ];
+    }
+}
