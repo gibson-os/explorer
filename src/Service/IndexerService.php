8ed7617d14329cc6f1b6b70e85f06e62f6678a05
Benjamin Wollenweber <ich@wollis-page.de>
2021-05-17 21:35:19 +0200

    Update idea git ignore

---
+++ src/Service/IndexerService.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace GibsonOS\Module\Explorer\Service;
+
+use GibsonOS\Module\Explorer\Dto\Dir;
+use GibsonOS\Module\Explorer\Dto\File;
+
+class IndexerService
+{
+    public function indexDir(Dir $dir): void
+    {
+    }
+
+    public function indexFile(File $file): void
+    {
+    }
+}
