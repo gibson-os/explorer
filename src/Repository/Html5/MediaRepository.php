2cc23b155b75cf138ec10ad797c91ae9e74d546b
Benjamin Wollenweber <ich@wollis-page.de>
2021-10-09 00:08:22 +0200

    CHANGE use

--- src/Repository/Html5/MediaRepository.php
+++ src/Repository/Html5/MediaRepository.php
@@ -7,7 +7,7 @@ use DateTime;
 use GibsonOS\Core\Exception\DateTimeError;
 use GibsonOS\Core\Exception\Repository\SelectError;
 use GibsonOS\Core\Repository\AbstractRepository;
-use GibsonOS\Module\Explorer\Model\Html5\Media as MediaModel;
+use GibsonOS\Module\Explorer\Model\Html5\Media;
 
 /**
  * @psalm-suppress MoreSpecificReturnType
@@ -19,11 +19,11 @@ class MediaRepository extends AbstractRepository
      * @throws DateTimeError
      * @throws SelectError
      */
-    public function getByToken(string $token): MediaModel
+    public function getByToken(string $token): Media
     {
-        $model = $this->fetchOne('`token`=?', [$token], MediaModel::class);
+        $model = $this->fetchOne('`token`=?', [$token], Media::class);
 
-        if (!$model instanceof MediaModel) {
+        if (!$model instanceof Media) {
             throw new SelectError();
         }
 
@@ -31,17 +31,17 @@ class MediaRepository extends AbstractRepository
     }
 
     /**
+     *@throws SelectError
      * @throws DateTimeError
-     * @throws SelectError
      *
-     * @return MediaModel[]
+     * @return Media[]
      */
     public function getByTokens(array $tokens): array
     {
         return $this->fetchAll(
             '`token` IN (' . implode(', ', array_fill(0, count($tokens), '?')) . ')',
             $tokens,
-            MediaModel::class
+            Media::class
         );
     }
 
@@ -49,11 +49,11 @@ class MediaRepository extends AbstractRepository
      * @throws DateTimeError
      * @throws SelectError
      */
-    public function getByDirAndFilename(string $dir, string $filename): MediaModel
+    public function getByDirAndFilename(string $dir, string $filename): Media
     {
-        $model = $this->fetchOne('`dir`=? AND `filename`=?', [$dir, $filename], MediaModel::class);
+        $model = $this->fetchOne('`dir`=? AND `filename`=?', [$dir, $filename], Media::class);
 
-        if (!$model instanceof MediaModel) {
+        if (!$model instanceof Media) {
             throw new SelectError();
         }
 
@@ -61,28 +61,51 @@ class MediaRepository extends AbstractRepository
     }
 
     /**
+     *@throws SelectError
      * @throws DateTimeError
-     * @throws SelectError
      *
-     * @return MediaModel[]
+     * @return Media[]
      */
     public function getAllByStatus(string $status): array
     {
-        return $this->fetchAll('`status`=?', [$status], MediaModel::class);
+        return $this->fetchAll('`status`=?', [$status], Media::class);
     }
 
     /**
-     * @throws DateTimeError
      * @throws SelectError
+     * @throws DateTimeError
      *
-     * @return MediaModel[]
+     * @return Media[]
+     */
+    public function getAllGeneratedOrderByViewed(): array
+    {
+        $mediaTableName = Media::getTableName();
+        $mediaPositionTableName = Media::getTableName();
+        $table = $this->getTable($mediaTableName)
+            ->setWhere('`status`=?')
+            ->addWhereParameter(Media::STATUS_GENERATED)
+            ->appendJoin(
+                $mediaPositionTableName,
+                '`' . $mediaTableName . '`.`id`=`' . $mediaPositionTableName . '`.`media_id`'
+            )
+            ->setOrderBy()
+        ;
+
+        return $this->getModels($table, Media::class);
+    }
+
+    /**
+     *@throws SelectError
+     * @throws DateTimeError
+     *
+     * @return Media[]
      */
     public function getAllOlderThan(DateTime $date): array
     {
         return $this->fetchAll(
             '`added`<?',
             [$date->format('Y-m-d H:i:s')],
-            MediaModel::class
+            Media::class
         );
     }
 
@@ -94,7 +117,7 @@ class MediaRepository extends AbstractRepository
         $token = md5((string) rand());
 
         try {
-            $this->fetchOne('`token`=?', [$token], MediaModel::class);
+            $this->fetchOne('`token`=?', [$token], Media::class);
 
             return $this->getFreeToken();
         } catch (SelectError $e) {
