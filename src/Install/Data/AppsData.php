71bc62dbde14306d860ad69c2e887462d296d90f
Benjamin Wollenweber <ich@wollis-page.de>
2022-01-20 17:29:13 +0100

    Add install apps

---
+++ src/Install/Data/AppsData.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace GibsonOS\Module\Explorer\Install\Data;
+
+use Generator;
+use GibsonOS\Core\Dto\Install\Success;
+use GibsonOS\Core\Install\AbstractInstall;
+use GibsonOS\Core\Service\InstallService;
+use GibsonOS\Core\Service\PriorityInterface;
+
+class AppsData extends AbstractInstall implements PriorityInterface
+{
+    public function install(string $module): Generator
+    {
+        $this
+            ->addApp('Explorer', 'explorer', 'index', 'index', 'icon_dir')
+            ->addApp('Papierkorb', 'explorer', 'trash', 'index', 'icon_trash')
+            ->addApp('HTML5 Medien', 'explorer', 'html5', 'index', 'icon_html5')
+        ;
+
+        yield new Success('Explorer apps installed!');
+    }
+
+    public function getPart(): string
+    {
+        return InstallService::PART_DATA;
+    }
+
+    public function getModule(): ?string
+    {
+        return 'explorer';
+    }
+
+    public function getPriority(): int
+    {
+        return 0;
+    }
+}
