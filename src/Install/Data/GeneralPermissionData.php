9f36c6c5d60b808d47c9e51b21940e721c076bf2
Benjamin Wollenweber <ich@wollis-page.de>
2022-01-20 14:22:37 +0100

    Fix set permission

--- src/Install/Data/GeneralPermissionData.php
+++ src/Install/Data/GeneralPermissionData.php
@@ -5,17 +5,23 @@ namespace GibsonOS\Module\Explorer\Install\Data;
 
 use Generator;
 use GibsonOS\Core\Dto\Install\Success;
-use GibsonOS\Core\Exception\Model\SaveError;
+use GibsonOS\Core\Exception\Repository\SelectError;
 use GibsonOS\Core\Install\AbstractInstall;
 use GibsonOS\Core\Model\User\Permission;
+use GibsonOS\Core\Repository\User\PermissionRepository;
 use GibsonOS\Core\Service\InstallService;
 use GibsonOS\Core\Service\PriorityInterface;
+use GibsonOS\Core\Service\ServiceManagerService;
 
 class GeneralPermissionData extends AbstractInstall implements PriorityInterface
 {
-    /**
-     * @throws SaveError
-     */
+    public function __construct(
+        ServiceManagerService $serviceManagerService,
+        private PermissionRepository $permissionRepository
+    ) {
+        parent::__construct($serviceManagerService);
+    }
+
     public function install(string $module): Generator
     {
         $this
@@ -33,13 +39,17 @@ class GeneralPermissionData extends AbstractInstall implements PriorityInterface
 
     private function setPermission(string $action): GeneralPermissionData
     {
-        (new Permission())
-            ->setModule('explorer')
-            ->setTask('html5')
-            ->setAction($action)
-            ->setPermission(Permission::READ)
-            ->save()
-        ;
+        try {
+            $this->permissionRepository->getByModuleTaskAndAction('explorer', 'html5', $action);
+        } catch (SelectError) {
+            (new Permission())
+                ->setModule('explorer')
+                ->setTask('html5')
+                ->setAction($action)
+                ->setPermission(Permission::READ)
+                ->save()
+            ;
+        }
 
         return $this;
     }
